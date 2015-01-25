4c3cfa36d3a8a82da6620264ac33dcfd7b7210d3
laplace22 <toh@zj8.so-net.ne.jp>
2015-01-25 16:17:45 +0900

    refine variable names

--- xdokodemo.c
+++ xdokodemo.c
@@ -46,73 +46,73 @@ int chgkey(int mtkey)
         case VK_CONVERT:    return XK_Henkan;
         case VK_NONCONVERT: return XK_Muhenkan;
 //      case VK_JUNJA, VK_FINAL, VK_ACCEPT, VK_MODECHANGE
-        case VK_ESCAPE:    return XK_Escape;
-        case VK_PRIOR:     return XK_Prior;
-        case VK_NEXT:      return XK_Next;
-        case VK_END:       return XK_End;
-        case VK_HOME:      return XK_Home;
-        case VK_LEFT:      return XK_Left;
-        case VK_UP:        return XK_Up;
-        case VK_RIGHT:     return XK_Right;
-        case VK_DOWN:      return XK_Down;
-        case VK_PRINT:     return XK_Print;
-        case VK_SNAPSHOT:  return XK_Sys_Req;
-        case VK_INSERT:    return XK_Insert;
-        case VK_DELETE:    return XK_Delete;
-        case VK_HELP:      return XK_Help;
-        case VK_LWIN:      return XK_F13;
-        case VK_RWIN:      return XK_Meta_R;
-        case VK_NUMPAD0:   return XK_KP_0;
-        case VK_NUMPAD1:   return XK_KP_1;
-        case VK_NUMPAD2:   return XK_KP_2;
-        case VK_NUMPAD3:   return XK_KP_3;
-        case VK_NUMPAD4:   return XK_KP_4;
-        case VK_NUMPAD5:   return XK_KP_5;
-        case VK_NUMPAD6:   return XK_KP_6;
-        case VK_NUMPAD7:   return XK_KP_7;
-        case VK_NUMPAD8:   return XK_KP_8;
-        case VK_NUMPAD9:   return XK_KP_9;
-        case VK_MULTIPLY:  return XK_KP_Multiply;
-        case VK_ADD:       return XK_KP_Add;
-        case VK_SEPARATOR: return XK_KP_Separator;
-        case VK_SUBTRACT:  return XK_KP_Subtract;
-        case VK_DECIMAL:   return XK_KP_Decimal;
-        case VK_DIVIDE:    return XK_KP_Divide;
-        case VK_F1:        return XK_F1;
-        case VK_F2:        return XK_F2;
-        case VK_F3:        return XK_F3;
-        case VK_F4:        return XK_F4;
-        case VK_F5:        return XK_F5;
-        case VK_F6:        return XK_F6;
-        case VK_F7:        return XK_F7;
-        case VK_F8:        return XK_F8;
-        case VK_F9:        return XK_F9;
-        case VK_F10:       return XK_F10;
-        case VK_F11:       return XK_F11;
-        case VK_F12:       return XK_F12;
-        case VK_NUMLOCK:   return XK_Num_Lock;
-        case VK_SCROLL:    return XK_Scroll_Lock;
-        case VK_LSHIFT:    return XK_Shift_L;
-        case VK_RSHIFT:    return XK_Shift_R;
-        case VK_LCONTROL:  return XK_Control_L;
-        case VK_RCONTROL:  return XK_Control_R;
-        case VK_LMENU:     return XK_Alt_L;
-        case VK_RMENU:     return XK_Alt_R;
+        case VK_ESCAPE:     return XK_Escape;
+        case VK_PRIOR:      return XK_Prior;
+        case VK_NEXT:       return XK_Next;
+        case VK_END:        return XK_End;
+        case VK_HOME:       return XK_Home;
+        case VK_LEFT:       return XK_Left;
+        case VK_UP:         return XK_Up;
+        case VK_RIGHT:      return XK_Right;
+        case VK_DOWN:       return XK_Down;
+        case VK_PRINT:      return XK_Print;
+        case VK_SNAPSHOT:   return XK_Sys_Req;
+        case VK_INSERT:     return XK_Insert;
+        case VK_DELETE:     return XK_Delete;
+        case VK_HELP:       return XK_Help;
+        case VK_LWIN:       return XK_F13;
+        case VK_RWIN:       return XK_Meta_R;
+        case VK_NUMPAD0:    return XK_KP_0;
+        case VK_NUMPAD1:    return XK_KP_1;
+        case VK_NUMPAD2:    return XK_KP_2;
+        case VK_NUMPAD3:    return XK_KP_3;
+        case VK_NUMPAD4:    return XK_KP_4;
+        case VK_NUMPAD5:    return XK_KP_5;
+        case VK_NUMPAD6:    return XK_KP_6;
+        case VK_NUMPAD7:    return XK_KP_7;
+        case VK_NUMPAD8:    return XK_KP_8;
+        case VK_NUMPAD9:    return XK_KP_9;
+        case VK_MULTIPLY:   return XK_KP_Multiply;
+        case VK_ADD:        return XK_KP_Add;
+        case VK_SEPARATOR:  return XK_KP_Separator;
+        case VK_SUBTRACT:   return XK_KP_Subtract;
+        case VK_DECIMAL:    return XK_KP_Decimal;
+        case VK_DIVIDE:     return XK_KP_Divide;
+        case VK_F1:         return XK_F1;
+        case VK_F2:         return XK_F2;
+        case VK_F3:         return XK_F3;
+        case VK_F4:         return XK_F4;
+        case VK_F5:         return XK_F5;
+        case VK_F6:         return XK_F6;
+        case VK_F7:         return XK_F7;
+        case VK_F8:         return XK_F8;
+        case VK_F9:         return XK_F9;
+        case VK_F10:        return XK_F10;
+        case VK_F11:        return XK_F11;
+        case VK_F12:        return XK_F12;
+        case VK_NUMLOCK:    return XK_Num_Lock;
+        case VK_SCROLL:     return XK_Scroll_Lock;
+        case VK_LSHIFT:     return XK_Shift_L;
+        case VK_RSHIFT:     return XK_Shift_R;
+        case VK_LCONTROL:   return XK_Control_L;
+        case VK_RCONTROL:   return XK_Control_R;
+        case VK_LMENU:      return XK_Alt_L;
+        case VK_RMENU:      return XK_Alt_R;
         //common keys
-        case 188:          return XK_comma;        // ,
-        case 189:          return XK_minus;        // -
-        case 190:          return XK_period;       // .
-        case 191:          return XK_slash;        // /
-        case 219:          return XK_bracketleft;  // [
-        case 220:          return XK_bar;          // yen
-        case 221:          return XK_bracketright; // ]
-        case 226:          return XK_underscore;   // backslash
-        case 186:          return XK_colon;        // :
-        case 187:          return XK_semicolon;    // ;
-        case 192:          return XK_at;           // @
-        case 222:          return XK_asciicircum;  // ^
+        case 188:           return XK_comma;        // ,
+        case 189:           return XK_minus;        // -
+        case 190:           return XK_period;       // .
+        case 191:           return XK_slash;        // /
+        case 219:           return XK_bracketleft;  // [
+        case 220:           return XK_bar;          // yen
+        case 221:           return XK_bracketright; // ]
+        case 226:           return XK_underscore;   // backslash
+        case 186:           return XK_colon;        // :
+        case 187:           return XK_semicolon;    // ;
+        case 192:           return XK_at;           // @
+        case 222:           return XK_asciicircum;  // ^
         //Windows fitting
-        case 244:          return XK_Kanji;        // hankaku_zenkaku
+        case 244:           return XK_Kanji;        // hankaku_zenkaku
     }
     return mtkey;
 }
@@ -138,12 +138,17 @@ int main(int argc, char *argv[], char *envp[]) {
     struct sockaddr_in sockAddr;
 
     // 各種パラメータ
-    int tmp,tmp2;
-    char* tmpp;
-    int status;
-    int numrcv;
-    unsigned int addr;
-    char buffer[BUFFER_SIZE];
+    int          args_ptr            ;
+    int          packet_size         ;
+    int          button_num          ;
+    int          key_code            ;
+    int          system_ret          ;
+    int          slash_ptr           ;
+    char*        char_ptr            ;
+    int          status              ;
+    int          numrcv              ;
+    unsigned int addr                ;
+    char         buffer[BUFFER_SIZE] ;
 
     // for mask
     struct in_addr net,mask;
@@ -157,33 +162,33 @@ int main(int argc, char *argv[], char *envp[]) {
     struct command packet;
 
     //コマンドラインオプションチェック
-    for(tmp=1;tmp<argc;tmp++){
-        if(argv[tmp][0]=='-'){
-            switch(argv[tmp][1]){
+    for(args_ptr=1;args_ptr<argc;args_ptr++){
+        if(argv[args_ptr][0]=='-'){
+            switch(argv[args_ptr][1]){
             case 'f'://from アドレスチェック
-                if(argv[tmp][2]=='\0'){
-                    tmpp=argv[tmp+1];
-                    tmp++;
+                if(argv[args_ptr][2]=='\0'){
+                    char_ptr=argv[args_ptr+1];
+                    args_ptr++;
                 }else{
-                    tmpp=argv[tmp]+2;
+                    char_ptr=argv[args_ptr]+2;
                 }
-                if (strchr(tmpp,'/')){
+                if (strchr(char_ptr,'/')){
                     //maskあり
-                    tmp2=atoi(strchr(tmpp,'/')+1);
-                    if (tmp2<1||tmp2>32){
+                    slash_ptr=atoi(strchr(char_ptr,'/')+1);
+                    if (slash_ptr<1||slash_ptr>32){
                         perror("Ip address error.(mask)");
                         return 1;
                     }
-                    mask.s_addr=-1<<(32-tmp2);
-                    *strchr(tmpp,'/')='\0';
-                    if (!inet_aton(tmpp,&net)){
-                        printf("%s",tmpp);
+                    mask.s_addr=-1<<(32-slash_ptr);
+                    *strchr(char_ptr,'/')='\0';
+                    if (!inet_aton(char_ptr,&net)){
+                        printf("%s",char_ptr);
                         perror("Ip address error.(addr)");
                         return 1;
                     }
                 }else{
                     //maskなし
-                    if (!inet_aton(tmpp,&net)){
+                    if (!inet_aton(char_ptr,&net)){
                         perror("Ip address error.(addr)");
                         return 1;
                     }
@@ -191,11 +196,11 @@ int main(int argc, char *argv[], char *envp[]) {
                 }
                 break;
             case 'p'://ポート指定
-                if(argv[tmp][2]=='\0'){
-                    port=atoi(argv[tmp+1]);
-                    tmp++;
+                if(argv[args_ptr][2]=='\0'){
+                    port=atoi(argv[args_ptr+1]);
+                    args_ptr++;
                 }else{
-                    port=atoi(argv[tmp]+2);
+                    port=atoi(argv[args_ptr]+2);
                 }
                 if(port==0)port=34600;
                 break;
@@ -233,8 +238,8 @@ int main(int argc, char *argv[], char *envp[]) {
     /* パケット受信 */
     while(1) {
         //受信処理
-        tmp=sizeof(struct sockaddr_in);
-        numrcv=recvfrom(recvSocket,buffer,(size_t)BUFFER_SIZE,0,(struct sockaddr*)&sockAddr,(socklen_t *)&tmp);
+        packet_size=sizeof(struct sockaddr_in);
+        numrcv=recvfrom(recvSocket,buffer,(size_t)BUFFER_SIZE,0,(struct sockaddr*)&sockAddr,(socklen_t *)&packet_size);
         if(numrcv==-1){
             status=close(recvSocket);
             break;
@@ -249,7 +254,7 @@ int main(int argc, char *argv[], char *envp[]) {
                     packet.cmd=1;
                     sendto(recvSocket,&packet,(size_t)sizeof(packet),0,(struct sockaddr *)&sockAddr,(socklen_t)sizeof(sockAddr));
                     if(b_dpms){
-                        system("xset dpms force on");//ディスプレイ省電力停止
+                        system_ret=system("xset dpms force on");//ディスプレイ省電力停止
                     }
                     break;
 
@@ -264,17 +269,18 @@ int main(int argc, char *argv[], char *envp[]) {
                 case CMD_RIGHT_DOWN:
                 case CMD_MIDDLE_UP:
                 case CMD_MIDDLE_DOWN:
+                    button_num = 0;
                     switch ((packet.cmd-3)>>1){
-                        case 0: tmp=Button1; break;
-                        case 1: tmp=Button3; break;
-                        case 2: tmp=Button2; break;
+                        case 0: button_num = Button1; break;
+                        case 1: button_num = Button3; break;
+                        case 2: button_num = Button2; break;
                     }
                     XTestFakeMotionEvent(disp,-1,((int)packet.param1)*XDisplayWidth (disp,0)/65535,
                                                  ((int)packet.param2)*XDisplayHeight(disp,0)/65535,0);
                     if(packet.cmd&1){
-                        XTestFakeButtonEvent(disp,tmp,False,0);
+                        XTestFakeButtonEvent(disp,button_num,False,0);
                     }else{
-                        XTestFakeButtonEvent(disp,tmp,True,0);
+                        XTestFakeButtonEvent(disp,button_num,True, 0);
                     }
                     break;
 
@@ -290,21 +296,22 @@ int main(int argc, char *argv[], char *envp[]) {
 
                 case CMD_KEY_UP://キーボード関連
                 case CMD_KEY_DOWN:
-                    if((tmp=XKeysymToKeycode(disp,chgkey(packet.param1)))<=0)break;
-                    if(tmp==49){
-                        XTestFakeKeyEvent(disp,tmp,True,0);
-                        XTestFakeKeyEvent(disp,tmp,False,0);
+                    key_code = 0;
+                    if((key_code=XKeysymToKeycode(disp,chgkey(packet.param1)))<=0)break;
+                    if(key_code==49){
+                        XTestFakeKeyEvent(disp,key_code,True,0);
+                        XTestFakeKeyEvent(disp,key_code,False,0);
                     }else{
                         if(packet.cmd&1){
-                            XTestFakeKeyEvent(disp,tmp,False,0);
+                            XTestFakeKeyEvent(disp,key_code,False,0);
                         }else{
-                            XTestFakeKeyEvent(disp,tmp,True,0);
+                            XTestFakeKeyEvent(disp,key_code,True,0);
                         }
                     }
                     break;
             }
         }else{
-            packet.cmd=2;
+            packet.cmd=CMD_BYE;
             sendto(recvSocket,&packet,(size_t)sizeof(packet),0,(struct sockaddr *)&sockAddr,(socklen_t)sizeof(sockAddr));
         }
         XFlush(disp);
